fabf690ede8dda73791378c549bb2f9da988f69b
semon69 <smdemon3@gmail.com>
2023-06-08 15:00:55 +0600

    update instructor

--- index.js
+++ index.js
@@ -107,6 +107,16 @@ async function run() {
             const result = { admin: user?.role === 'admin' }
             res.send(result)
         })
+        app.get('/users/instructor/:email', verifyJWT, async (req, res) => {
+            const email = req.params.email;
+            if (req.decoded.email !== email) {
+                res.send({ instructor: false })
+            }
+            const query = { email: email }
+            const user = await usersCollection.findOne(query)
+            const result = { instructor: user?.role === 'instructor' }
+            res.send(result)
+        })
 
         app.patch('/users/admin/:id', async (req, res) => {
             const id = req.params.id;
@@ -114,6 +124,18 @@ async function run() {
             const updateDoc = {
                 $set: {
                     role: 'admin'
+                }
+            }
+            const result = await usersCollection.updateOne(filter, updateDoc)
+            res.send(result)
+        })
+        
+        app.patch('/users/instructor/:id', async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: new ObjectId(id) }
+            const updateDoc = {
+                $set: {
+                    role: 'instructor'
                 }
             }
             const result = await usersCollection.updateOne(filter, updateDoc)
